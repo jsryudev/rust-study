2788fa51038bb46cb8c59e6a8add4225b47c8628
JunSang Ryu <jsryu.dev@gmail.com>
2021-02-28 21:57:22 +0900

    Update restaurant example

--- book/restaurant/src/lib.rs
+++ book/restaurant/src/lib.rs
@@ -1,25 +1,24 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+  mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
     }
 }
+
 pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
-    front_of_house::hosting::add_to_waitlist();
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 }
-
-fn serve_order() {}
-
-mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::serve_order();
-    }
-
-    fn cook_order() {}
-}
-
-
 
 #[cfg(test)]
 mod tests {
