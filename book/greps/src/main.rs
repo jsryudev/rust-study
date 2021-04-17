72c2f09af60ce73464361d3a3fbbbfd71178f77c
JunSang Ryu <jsryu.dev@gmail.com>
2021-04-17 22:06:36 +0900

    Update greps example using iter

--- book/greps/src/main.rs
+++ book/greps/src/main.rs
@@ -6,7 +6,7 @@ use std::process;
 use greps::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    //let args: Vec<String> = env::args().collect();
     
     //let query = &args[1];
     //let filename = &args[2];
@@ -29,10 +29,16 @@ fn main() {
     //    process::exit(1);
     //}
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
+    //let config = Config::new(&args).unwrap_or_else(|err| {
+    //    eprintln!("Problem parsing arguments: {}", err);
+    //    process::exit(1);
+    //});
+    
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
         process::exit(1);
-    });
+    })
+    
 
     if let Err(e) = greps::run(config) {
         eprintln!("Application error: {}", e);
