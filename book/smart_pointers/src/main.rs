a53a80403875fdefe6bb59de5b03a7289090d0d0
JunSang Ryu <jsryu.dev@gmail.com>
2021-04-18 21:12:07 +0900

    Add smart_pointers example

---
+++ book/smart_pointers/src/main.rs
@@ -0,0 +1,16 @@
+use List::{Cons, Nil};
+
+fn main() {
+    //let b = Box::new(5);
+    //println!("b = {}", b);
+
+    let list = Cons(1,
+        Box::new(Cons(2,
+            Box::new(Cons(3,
+                Box::new(Nil))))));
+}
+
+enum List {
+    Cons(i32, List),
+    Nil,
+}
