7c4015604e5e4c6eb010ee7390e761a42b6c102b
JunSang Ryu <jsryu.dev@gmail.com>
2021-04-19 23:31:29 +0900

    Update smart_pointers example

--- book/smart_pointers/src/main.rs
+++ book/smart_pointers/src/main.rs
@@ -1,16 +1,58 @@
-use List::{Cons, Nil};
+//use List::{Cons, Nil};
+
+//enum List {
+//    Cons(i32, List),
+//    Nil,
+//}
 
 fn main() {
     //let b = Box::new(5);
     //println!("b = {}", b);
 
-    let list = Cons(1,
-        Box::new(Cons(2,
-            Box::new(Cons(3,
-                Box::new(Nil))))));
+    //let list = Cons(1,
+    //    Box::new(Cons(2,
+    //        Box::new(Cons(3,
+    //            Box::new(Nil))))));
+
+    //let x = 5;
+    //let y = &x;
+            
+    //assert_eq!(5, x);
+    //assert_eq!(5, *y);
+
+    //let x = 5;
+    //let y = Box::new(x);
+
+    //assert_eq!(5, x);
+    //assert_eq!(5, *y);
+
+    //let x = 5;
+    //let y = MyBox::new(x);
+
+    //assert_eq!(5, x);
+    //assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
 
-enum List {
-    Cons(i32, List),
-    Nil,
+use std::ops::Deref;
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }
