968c2c78d7a0e8d8062cf58a4520066573caac5b
JunSang Ryu <jsryu.dev@gmail.com>
2021-04-20 23:26:16 +0900

    Update smart_pointers example

--- book/smart_pointers/src/main.rs
+++ book/smart_pointers/src/main.rs
@@ -5,6 +5,21 @@
 //    Nil,
 //}
 
+//enum List {
+//    Cons(i32, Box<List>),
+//    Nil,
+//}
+
+//use List::{Cons, Nil};
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+use std::rc::Rc;
+
 fn main() {
     //let b = Box::new(5);
     //println!("b = {}", b);
@@ -39,11 +54,25 @@ fn main() {
     //let d = CustomSmartPointer { data: String::from("other stuff") };
     //println!("CustomSmartPointers created.");
 
-    let c = CustomSmartPointer { data: String::from("some data") };
-    println!("CustomSmartPointer created.");
+    //let c = CustomSmartPointer { data: String::from("some data") };
+    //println!("CustomSmartPointer created.");
     //c.drop();
-    drop(c);
-    println!("CustomSmartPointer dropped before the end of main.");
+    //drop(c);
+    //println!("CustomSmartPointer dropped before the end of main.");
+
+    //let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    //let b = Cons(3, Rc::clone(&a));
+    //let c = Cons(4, Rc::clone(&a));
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
 //use std::ops::Deref;
@@ -67,12 +96,12 @@ fn main() {
 //    println!("Hello, {}!", name);
 //}
 
-struct CustomSmartPointer {
-    data: String,
-}
+//struct CustomSmartPointer {
+//    data: String,
+//}
 
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
-    }
-}
+//impl Drop for CustomSmartPointer {
+//    fn drop(&mut self) {
+//        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+//    }
+//}
