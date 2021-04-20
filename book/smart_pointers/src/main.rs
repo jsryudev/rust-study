a51c2ecb860f0fcae45b583cc48abe81741fa76d
JunSang Ryu <jsryu.dev@gmail.com>
2021-04-20 22:58:34 +0900

    Update smart_pointers example

--- book/smart_pointers/src/main.rs
+++ book/smart_pointers/src/main.rs
@@ -32,27 +32,47 @@ fn main() {
     //assert_eq!(5, x);
     //assert_eq!(5, *y);
 
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
+    //let m = MyBox::new(String::from("Rust"));
+    //hello(&m);
+
+    //let c = CustomSmartPointer { data: String::from("my stuff") };
+    //let d = CustomSmartPointer { data: String::from("other stuff") };
+    //println!("CustomSmartPointers created.");
+
+    let c = CustomSmartPointer { data: String::from("some data") };
+    println!("CustomSmartPointer created.");
+    //c.drop();
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
 }
 
-use std::ops::Deref;
-struct MyBox<T>(T);
+//use std::ops::Deref;
+//struct MyBox<T>(T);
 
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
+//impl<T> MyBox<T> {
+//    fn new(x: T) -> MyBox<T> {
+//        MyBox(x)
+//    }
+//}
+
+//impl<T> Deref for MyBox<T> {
+//    type Target = T;
+
+//    fn deref(&self) -> &T {ㄴ
+//        &self.0
+//    }
+//}
+
+//fn hello(name: &str) {
+//    println!("Hello, {}!", name);
+//}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
     }
-}
-
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
-    }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {}!", name);
 }
